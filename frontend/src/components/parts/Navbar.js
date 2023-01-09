e04eed112eb27c20e2e518e07ddedf00bf0de4f3
nurarifin256 <arifinnur402@gmail.com>
2023-01-09 15:13:11 +0700

    install bootstrap dan membuat navbar

---
+++ frontend/src/components/parts/Navbar.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand">HRIS</a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page">
+                Home
+              </a>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Employes
+              </a>
+              <ul className="dropdown-menu">
+                <li>
+                  <a className="dropdown-item">employee</a>
+                </li>
+
+                <li>
+                  <a className="dropdown-item">Departmen</a>
+                </li>
+
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Position
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
