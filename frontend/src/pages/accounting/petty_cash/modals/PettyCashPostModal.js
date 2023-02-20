e07c84ca2da2a9a9201d903da2c495e597c818cb
nurarifin256 <arifinnur402@gmail.com>
2023-02-20 11:20:44 +0700

    membuat fungsi onClick tambah element input baru

--- frontend/src/pages/accounting/petty_cash/modals/PettyCashPostModal.js
+++ frontend/src/pages/accounting/petty_cash/modals/PettyCashPostModal.js
@@ -4,15 +4,28 @@ import CurrencyFormat from "react-currency-format";
 import "../style.css";
 
 const PettyCashPostModal = ({ coas, department, postRefill, refetch }) => {
+  const [numberInvoice, setNumberInvoice] = useState("");
+  const [errorInvoice, setErrorInvoice] = useState("");
+
   const [idCoa, setIdCoa] = useState("");
   const [idDepartment, setIdDepartment] = useState("");
   const [description, setDescription] = useState("");
-  const [debit, setDebit] = useState("");
+  const [debit, setDebit] = useState([
+    {
+      number: "",
+      error: null,
+    },
+  ]);
 
   const [idCoaC, setIdCoaC] = useState("");
   const [idDepartmentC, setIdDepartmentC] = useState("");
   const [descriptionC, setDescriptionC] = useState("");
-  const [credit, setCredit] = useState("");
+  const [credit, setCredit] = useState([
+    {
+      number: "",
+      error: null,
+    },
+  ]);
 
   const [errorCoa, setErrorCoa] = useState("");
   const [errorDepartment, setErrorDepartment] = useState("");
@@ -24,18 +37,7 @@ const PettyCashPostModal = ({ coas, department, postRefill, refetch }) => {
   const [errorDescriptionC, setErrorDescriptionC] = useState("");
   const [errorCredit, setErrorCredit] = useState("");
 
-  //   const [previewUrl, setPreviewUrl] = useState(null);
   const [images, setImages] = useState([]);
-
-  const handleImageChange = (event) => {
-    const selectedFIles = [];
-    const targetFiles = event.target.files;
-    const targetFilesObject = [...targetFiles];
-    targetFilesObject.map((file) => {
-      return selectedFIles.push(URL.createObjectURL(file));
-    });
-    setImages(selectedFIles);
-  };
 
   const optionsCoa = coas.map((item) => {
     return {
@@ -50,6 +52,50 @@ const PettyCashPostModal = ({ coas, department, postRefill, refetch }) => {
       value: item.id,
     };
   });
+
+  const handleImageChange = (event) => {
+    const selectedFIles = [];
+    const targetFiles = event.target.files;
+    const targetFilesObject = [...targetFiles];
+    targetFilesObject.map((file) => {
+      return selectedFIles.push(URL.createObjectURL(file));
+    });
+    setImages(selectedFIles);
+  };
+
+  const addDebit = () => {
+    const newDebit = [...debit, { number: "", error: null }];
+    setDebit(newDebit);
+  };
+
+  const deleteDebit = () => {
+    const newDebit = [...debit];
+    newDebit.pop();
+    setDebit(newDebit);
+  };
+
+  const handleDebitChange = (e, index) => {
+    const newDebit = [...debit];
+    newDebit[index].number = e.target.value;
+    setDebit(newDebit);
+  };
+
+  const addCredit = () => {
+    const newCredit = [...credit, { number: "", error: null }];
+    setCredit(newCredit);
+  };
+
+  const deleteCredit = () => {
+    const newCredit = [...credit];
+    newCredit.pop();
+    setCredit(newCredit);
+  };
+
+  const handleCreditChange = (e, index) => {
+    const newCredit = [...credit];
+    newCredit[index].number = e.target.value;
+    setCredit(newCredit);
+  };
 
   return (
     <div>
@@ -78,6 +124,7 @@ const PettyCashPostModal = ({ coas, department, postRefill, refetch }) => {
                 <label htmlFor="address" className="form-label">
                   Attachment
                 </label>
+
                 <div>
                   {images.map((url, i) => {
                     return (
@@ -89,6 +136,7 @@ const PettyCashPostModal = ({ coas, department, postRefill, refetch }) => {
                     );
                   })}
                 </div>
+
                 <input
                   className="form-control"
                   type="file"
@@ -100,107 +148,170 @@ const PettyCashPostModal = ({ coas, department, postRefill, refetch }) => {
               </div>
 
               <div className="mb-3">
-                <label className="form-label">Account number - name</label>
-                <Select
-                  placeholder="Choose COA"
-                  className={errorCoa ? "is-invalid" : null}
-                  onChange={(e) => setIdCoa(e.value)}
-                  options={optionsCoa}
-                />
-                {errorCoa && <span className="text-danger"> {errorCoa} </span>}
-              </div>
-
-              <div className="mb-3">
-                <label className="form-label">Position - Department</label>
-                <Select
-                  placeholder="Choose Department"
-                  className={errorDepartment ? "is-invalid" : null}
-                  onChange={(e) => setIdDepartment(e.value)}
-                  options={optionsDepartment}
-                />
-                {errorDepartment && (
-                  <span className="text-danger"> {errorDepartment} </span>
-                )}
-              </div>
-
-              <div className="mb-3">
-                <label className="form-label">Description</label>
-                <textarea
+                <label htmlFor="name" className="form-label">
+                  Invoice Number
+                </label>
+                <input
+                  value={numberInvoice}
+                  onChange={(e) => setNumberInvoice(e.target.value)}
+                  type="text"
                   className={`form-control ${
-                    errorDescription ? "is-invalid" : null
+                    errorInvoice ? "is-invalid" : null
                   }`}
-                  placeholder="Enter description"
-                  onChange={(e) => setDescription(e.target.value)}
-                ></textarea>
-              </div>
-
-              <div className="mb-3">
-                <label className="form-label">Debit</label>
-                <CurrencyFormat
-                  className={`form-control ${errorDebit ? "is-invalid" : null}`}
-                  thousandSeparator={true}
-                  placeholder="Enter Debit"
-                  onChange={(e) => setDebit(e.target.value)}
+                  id="name"
+                  placeholder="Enter invoice number"
                 />
-                {errorDebit && (
-                  <span className="text-danger"> {errorDebit} </span>
+                {errorInvoice && (
+                  <span className="text-danger"> {errorInvoice} </span>
                 )}
               </div>
 
               <hr />
 
-              <div className="mb-3 mt-3">
-                <label className="form-label">Account number - name</label>
-                <Select
-                  placeholder="Choose COA"
-                  className={errorCoaC ? "is-invalid" : null}
-                  onChange={(e) => setIdCoaC(e.value)}
-                  options={optionsCoa}
-                />
-                {errorCoaC && (
-                  <span className="text-danger"> {errorCoaC} </span>
-                )}
-              </div>
+              {debit.map((debit, index) => (
+                <div key={index}>
+                  <div className="mb-3">
+                    <label className="form-label">
+                      Account number - name {index + 1}
+                    </label>
+                    <Select
+                      placeholder="Choose COA"
+                      className={errorCoa ? "is-invalid" : null}
+                      onChange={(e) => setIdCoa(e.value)}
+                      options={optionsCoa}
+                    />
+                    {errorCoa && (
+                      <span className="text-danger"> {errorCoa} </span>
+                    )}
+                  </div>
 
-              <div className="mb-3">
-                <label className="form-label">Position - Department</label>
-                <Select
-                  placeholder="Choose Department"
-                  className={errorDepartmentC ? "is-invalid" : null}
-                  onChange={(e) => setIdDepartmentC(e.value)}
-                  options={optionsDepartment}
-                />
-                {errorDepartmentC && (
-                  <span className="text-danger"> {errorDepartmentC} </span>
-                )}
-              </div>
-              <div className="mb-3">
-                <label className="form-label">Description</label>
-                <textarea
-                  className={`form-control ${
-                    errorDescriptionC ? "is-invalid" : null
-                  }`}
-                  placeholder="Enter description"
-                  onChange={(e) => setDescriptionC(e.target.value)}
-                ></textarea>
-                {errorDescriptionC && (
-                  <span className="text-danger"> {errorDescriptionC} </span>
-                )}
-              </div>
-              <div className="mb-3">
-                <label className="form-label">Credit</label>
-                <CurrencyFormat
-                  className={`form-control ${
-                    errorCredit ? "is-invalid" : null
-                  }`}
-                  thousandSeparator={true}
-                  placeholder="Enter Credit"
-                  onChange={(e) => setCredit(e.target.value)}
-                />
-                {errorCredit && (
-                  <span className="text-danger"> {errorCredit} </span>
-                )}
-              </div>
+                  <div className="mb-3">
+                    <label className="form-label">
+                      Position - Department {index + 1}
+                    </label>
+                    <Select
+                      placeholder="Choose Department"
+                      className={errorDepartment ? "is-invalid" : null}
+                      onChange={(e) => setIdDepartment(e.value)}
+                      options={optionsDepartment}
+                    />
+                    {errorDepartment && (
+                      <span className="text-danger"> {errorDepartment} </span>
+                    )}
+                  </div>
+
+                  <div className="mb-3">
+                    <label className="form-label">
+                      Description {index + 1}
+                    </label>
+                    <textarea
+                      className={`form-control ${
+                        errorDescription ? "is-invalid" : null
+                      }`}
+                      placeholder="Enter description"
+                      onChange={(e) => setDescription(e.target.value)}
+                    ></textarea>
+                  </div>
+
+                  <div className="mb-3">
+                    <label className="form-label">Debit {index + 1}</label>
+                    <CurrencyFormat
+                      className={`form-control ${
+                        errorDebit ? "is-invalid" : null
+                      }`}
+                      thousandSeparator={true}
+                      placeholder="Enter Debit"
+                      onChange={(e) => handleDebitChange(e, index)}
+                    />
+                    {errorDebit && (
+                      <span className="text-danger"> {errorDebit} </span>
+                    )}
+                  </div>
+                </div>
+              ))}
+
+              <i
+                className="bi bi-dash btn btn-danger btn-sm"
+                onClick={() => deleteDebit()}
+              ></i>
+              <i
+                className="bi bi-plus btn btn-primary btn-sm ms-2"
+                onClick={() => addDebit()}
+              ></i>
+
+              <hr />
+
+              {credit.map((credit, index) => (
+                <div key={index}>
+                  <div className="mb-3 mt-3">
+                    <label className="form-label">
+                      Account number - name {index + 1}
+                    </label>
+                    <Select
+                      placeholder="Choose COA"
+                      className={errorCoaC ? "is-invalid" : null}
+                      onChange={(e) => setIdCoaC(e.value)}
+                      options={optionsCoa}
+                    />
+                    {errorCoaC && (
+                      <span className="text-danger"> {errorCoaC} </span>
+                    )}
+                  </div>
+
+                  <div className="mb-3">
+                    <label className="form-label">
+                      Position - Department {index + 1}
+                    </label>
+                    <Select
+                      placeholder="Choose Department"
+                      className={errorDepartmentC ? "is-invalid" : null}
+                      onChange={(e) => setIdDepartmentC(e.value)}
+                      options={optionsDepartment}
+                    />
+                    {errorDepartmentC && (
+                      <span className="text-danger"> {errorDepartmentC} </span>
+                    )}
+                  </div>
+                  <div className="mb-3">
+                    <label className="form-label">
+                      Description {index + 1}
+                    </label>
+                    <textarea
+                      className={`form-control ${
+                        errorDescriptionC ? "is-invalid" : null
+                      }`}
+                      placeholder="Enter description"
+                      onChange={(e) => setDescriptionC(e.target.value)}
+                    ></textarea>
+                    {errorDescriptionC && (
+                      <span className="text-danger"> {errorDescriptionC} </span>
+                    )}
+                  </div>
+                  <div className="mb-3">
+                    <label className="form-label">Credit {index + 1}</label>
+                    <CurrencyFormat
+                      className={`form-control ${
+                        errorCredit ? "is-invalid" : null
+                      }`}
+                      thousandSeparator={true}
+                      placeholder="Enter Credit"
+                      onChange={(e) => handleCreditChange(e, index)}
+                    />
+                    {errorCredit && (
+                      <span className="text-danger"> {errorCredit} </span>
+                    )}
+                  </div>
+                </div>
+              ))}
+
+              <i
+                className="bi bi-dash btn btn-danger btn-sm"
+                onClick={() => deleteCredit()}
+              ></i>
+              <i
+                className="bi bi-plus btn btn-primary btn-sm ms-2"
+                onClick={() => addCredit()}
+              ></i>
             </div>
             <div className="modal-footer">
               <button
